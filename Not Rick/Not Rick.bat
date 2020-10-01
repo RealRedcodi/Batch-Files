8576f9daa4731038b84696ae5c117e35b421aacd
Gimenez Killian <9Q.B7@pcsb.org>
2020-10-01 11:08:13 -0400

    Fixed download

--- Not Rick/Not Rick.bat
+++ Not Rick/Not Rick.bat
@@ -1,5 +1,5 @@
 @echo off
 cls
 echo Loading
-curl https://raw.githubusercontent.com/RealRedcodi/Short-Lib/master/Testing/Rick.mp4 -o rick.mp4
+curl https://raw.githubusercontent.com/RealRedcodi/Batch-Files/master/Not%20Rick/files/Rick.mp4 -o rick.mp4
 rick.mp4
